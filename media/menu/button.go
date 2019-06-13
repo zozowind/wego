93aba4bde00e5017af3a4ca299bb7ee6b057e773
陈致杰 <chenzhijie@heitouyang.cn>
2019-06-13 13:05:30 +0800

    更新menu button

--- media/menu/button.go
+++ media/menu/button.go
@@ -12,132 +12,111 @@ type Button struct {
 	SubButtons []*Button `json:"sub_button,omitempty"`
 }
 
-//SetSubButton 设置二级菜单
-func (btn *Button) SetSubButton(name string, subButtons []*Button) {
-	btn.Name = name
-	btn.SubButtons = subButtons
-	btn.Type = ""
-	btn.Key = ""
-	btn.URL = ""
-	btn.MediaID = ""
+//NewSubButton 二级菜单
+func NewSubButton(name string, subButtons []*Button) *Button {
+	return &Button{
+		Name:       name,
+		SubButtons: subButtons,
+	}
 }
 
-//SetClickButton btn 为click类型
-func (btn *Button) SetClickButton(name, key string) {
-	btn.Type = "click"
-	btn.Name = name
-	btn.Key = key
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewClickButton clict Btn类型
+func NewClickButton(name, key string) *Button {
+	return &Button{
+		Type: "click",
+		Key:  key,
+		Name: name,
+	}
 }
 
-//SetViewButton view类型
-func (btn *Button) SetViewButton(name, url string) {
-	btn.Type = "view"
-	btn.Name = name
-	btn.URL = url
-	btn.Key = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewViewButton view 类型Btn
+func NewViewButton(name, url string) *Button {
+	return &Button{
+		Type: "view",
+		Name: name,
+		URL:  url,
+	}
 }
 
-// SetScanCodePushButton 扫码推事件
-func (btn *Button) SetScanCodePushButton(name, key string) {
-	btn.Type = "scancode_push"
-	btn.Name = name
-	btn.Key = key
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewScanCodePushButton 扫码推事件
+func NewScanCodePushButton(name, key string) *Button {
+	return &Button{
+		Type: "scancode_push",
+		Name: name,
+		Key:  key,
+	}
 }
 
-//SetScanCodeWaitMsgButton 设置 扫码推事件且弹出"消息接收中"提示框
-func (btn *Button) SetScanCodeWaitMsgButton(name, key string) {
-	btn.Type = "scancode_waitmsg"
-	btn.Name = name
-	btn.Key = key
-
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewScanCodeWaitMsgButton  设置 扫码推事件且弹出"消息接收中"提示框
+func NewScanCodeWaitMsgButton(name, key string) *Button {
+	return &Button{
+		Type: "scancode_waitmsg",
+		Name: name,
+		Key:  key,
+	}
 }
 
-//SetPicSysPhotoButton 设置弹出系统拍照发图按钮
-func (btn *Button) SetPicSysPhotoButton(name, key string) {
-	btn.Type = "pic_sysphoto"
-	btn.Name = name
-	btn.Key = key
-
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewPicSysPhotoButton  设置弹出系统拍照发图按钮
+func NewPicSysPhotoButton(name, key string) *Button {
+	return &Button{
+		Type: "pic_sysphoto",
+		Name: name,
+		Key:  key,
+	}
 }
 
-//SetPicPhotoOrAlbumButton 设置弹出拍照或者相册发图类型按钮
-func (btn *Button) SetPicPhotoOrAlbumButton(name, key string) {
-	btn.Type = "pic_photo_or_album"
-	btn.Name = name
-	btn.Key = key
-
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewPicPhotoOrAlbumButton  设置弹出系统拍照发图按钮
+func NewPicPhotoOrAlbumButton(name, key string) *Button {
+	return &Button{
+		Type: "pic_photo_or_album",
+		Name: name,
+		Key:  key,
+	}
 }
 
-// SetPicWeixinButton 设置弹出微信相册发图器类型按钮
-func (btn *Button) SetPicWeixinButton(name, key string) {
-	btn.Type = "pic_weixin"
-	btn.Name = name
-	btn.Key = key
-
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewPicWeixinButton  设置弹出微信相册发图器类型按钮
+func NewPicWeixinButton(name, key string) *Button {
+	return &Button{
+		Type: "pic_weixin",
+		Name: name,
+		Key:  key,
+	}
 }
 
-// SetLocationSelectButton 设置 弹出地理位置选择器 类型按钮
-func (btn *Button) SetLocationSelectButton(name, key string) {
-	btn.Type = "location_select"
-	btn.Name = name
-	btn.Key = key
-
-	btn.URL = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewLocationSelectButton  设置 弹出地理位置选择器 类型按钮
+func NewLocationSelectButton(name, key string) *Button {
+	return &Button{
+		Type: "location_select",
+		Name: name,
+		Key:  key,
+	}
 }
 
-//SetMediaIDButton  设置 下发消息(除文本消息) 类型按钮
-func (btn *Button) SetMediaIDButton(name, mediaID string) {
-	btn.Type = "media_id"
-	btn.Name = name
-	btn.MediaID = mediaID
-
-	btn.Key = ""
-	btn.URL = ""
-	btn.SubButtons = nil
+//NewMediaIDButton  设置 下发消息(除文本消息) 类型按钮
+func NewMediaIDButton(name, mediaID string) *Button {
+	return &Button{
+		Type:    "media_id",
+		Name:    name,
+		MediaID: mediaID,
+	}
 }
 
-//SetViewLimitedButton  设置 跳转图文消息URL 类型按钮
-func (btn *Button) SetViewLimitedButton(name, mediaID string) {
-	btn.Type = "view_limited"
-	btn.Name = name
-	btn.MediaID = mediaID
-
-	btn.Key = ""
-	btn.URL = ""
-	btn.SubButtons = nil
+//NewViewLimitedButton  设置 跳转图文消息URL 类型按钮
+func NewViewLimitedButton(name, mediaID string) *Button {
+	return &Button{
+		Type:    "view_limited",
+		Name:    name,
+		MediaID: mediaID,
+	}
 }
 
-//SetMiniprogramButton  设置 跳转小程序 类型按钮 (公众号后台必须已经关联小程序)
-func (btn *Button) SetMiniprogramButton(name, url, appID, pagePath string) {
-	btn.Type = "miniprogram"
-	btn.Name = name
-	btn.URL = url
-	btn.AppID = appID
-	btn.PagePath = pagePath
-
-	btn.Key = ""
-	btn.MediaID = ""
-	btn.SubButtons = nil
+//NewMiniprogramButton  设置 跳转小程序 类型按钮 (公众号后台必须已经关联小程序)
+func NewMiniprogramButton(name, url, appID, pagePath string) *Button {
+	return &Button{
+		Type:     "miniprogram",
+		Name:     name,
+		URL:      url,
+		AppID:    appID,
+		PagePath: pagePath,
+	}
 }
