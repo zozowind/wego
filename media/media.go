346c05c51a1abf340ad2a61510f32e8bca33bb36
陈致杰 <chenzhijie@heitouyang.cn>
2020-01-13 20:21:49 +0800

    添加网页授权域名检查

--- media/media.go
+++ media/media.go
@@ -10,6 +10,7 @@ type WeMediaClient struct {
 	core.WeBase
 	TicketServer  TicketServer
 	MessageConfig *core.MessageConfig
+	AuthHosts     []string //业务认证域名
 
 	eventHandlers       map[message.EventType]func(*message.MixMessage) (*message.Reply, error)
 	defaultEventHandler func(*message.MixMessage) (*message.Reply, error)
