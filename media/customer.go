8acc4284e47d8a7ca7363a0fcc394b6f9134ee41
quan-ouyang <quan-ouyang@github.cn>
2020-11-03 18:51:54 +0800

    增加客服消息推送

---
+++ media/customer.go
@@ -0,0 +1,90 @@
+package media
+
+import (
+	"encoding/json"
+	"net/url"
+
+	"github.com/zozowind/wego/core"
+	"github.com/zozowind/wego/util"
+)
+
+//Customer .客服消息定义
+const (
+	TextMsgtype  = "text"
+	ImageMsgtype = "image"
+
+	customerSendURL = core.WxAPIURL + "/cgi-bin/message/custom/send"
+)
+
+//CustomersContentReq .文字信息请求参数
+type CustomersContentReq struct {
+	Touser  string               `json:"touser"`
+	Msgtype string               `json:"msgtype"`
+	Text    CustomersContentInfo `json:"text"`
+}
+
+//CustomersImageReq  图片信息请求参数
+type CustomersImageReq struct {
+	Touser  string             `json:"touser"`
+	Msgtype string             `json:"msgtype"`
+	Image   CustomersImageInfo `json:"image"`
+}
+
+//CustomersImageInfo .图片信息请求图片信息
+type CustomersImageInfo struct {
+	MediaID string `json:"media_id"`
+}
+
+//CustomersContentInfo .文本信息请求文本信息
+type CustomersContentInfo struct {
+	Content string `json:"content"`
+}
+
+//CustomerRsp .客服消息返回
+type CustomerRsp struct {
+	Errcode int    `json:"errcode"`
+	Errmsg  string `json:"errmsg"`
+}
+
+// SendCustomersContent 获取客服信息发送
+func (wm *WeMediaClient) SendCustomersContent(req *CustomersContentReq) (err error) {
+	token, err := wm.TokenServer.Token()
+	if nil != err {
+		return
+	}
+	params := url.Values{}
+	params.Set("access_token", token)
+
+	data, err := util.HTTPJsonPost(nil, customerSendURL+"?"+params.Encode(), req)
+	rsp := &core.WxErrorResponse{}
+	err = json.Unmarshal(data, rsp)
+	if nil != err {
+		return
+	}
+	err = rsp.Check()
+	if nil != err {
+		return
+	}
+	return
+}
+
+// SendCustomersPoster 获取客服图片发送
+func (wm *WeMediaClient) SendCustomersPoster(req *CustomersImageReq) (err error) {
+	token, err := wm.TokenServer.Token()
+	if nil != err {
+		return
+	}
+	params := url.Values{}
+	params.Set("access_token", token)
+	data, err := util.HTTPJsonPost(nil, customerSendURL+"?"+params.Encode(), req)
+	rsp := &core.WxErrorResponse{}
+	err = json.Unmarshal(data, rsp)
+	if nil != err {
+		return
+	}
+	err = rsp.Check()
+	if nil != err {
+		return
+	}
+	return
+}
